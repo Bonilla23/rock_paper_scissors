a9a31e9496425ed68501f37364efe8a115c7002d
Bonilla23 <ikerfernandezbonilla@gmail.com>
2025-01-29 13:48:03 +0100

    Create the text and its position

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,10 +6,15 @@ wn = turtle.Screen()
 wn.setup(width=600, height=600)  # Tamaño correcto
 wn.bgcolor("grey")
 
+# Crear el texto
+texto = turtle.Turtle()
+texto.penup()
+texto.goto(0,0)
+
 # Bucle del juego
 while True:
     action = wn.textinput("Input", "Introduce qué quieres hacer: ")  # Pedir entrada al usuario
     if action:  # Si el usuario introduce algo
-        print(f"Has introducido: {action}")
+        texto.write(f"Has introducido: {action}")
     
     time.sleep(1)  # Pequeña pausa para evitar saturación
