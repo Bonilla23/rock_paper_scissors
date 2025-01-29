c697c40fd603f8578a40b284effd43939e437973
Bonilla23 <ikerfernandezbonilla@gmail.com>
2025-01-29 13:54:57 +0100

    Import random to a random word or number

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 # Screen configuration
 wn = turtle.Screen()
@@ -17,6 +18,8 @@ while True:
     action = wn.textinput("Input", "Introduce qué quieres hacer: ")  #
     if action:  
         texto.write(f"Has introducido: {action}")
+        # We need a random number to select a rock,scissors and papaer
+        
     else:
         break # Exit the game if you say Cancel
     
